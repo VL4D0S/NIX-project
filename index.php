37729f861bd162402fba8f68eafa6fe3fd79689f
Kiva Vlad <vladkiva9@gmail.com>
2020-10-20 23:12:23 +0300

    task 3

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/style/style.css">
+    <title>NIX-project</title>
+</head>
+<body>
+    <header>
+        <div class="title-project">NIX-project</div>
+        <nav class="menu">
+            <a href="#"><li>Урок 1</li></a>
+            <a href="#"><li>Урок 2</li></a>
+        </nav>
+    </header>
+    <main>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente ullam adipisci autem eaque distinctio ut, similique et eligendi in quos cupiditate ipsum tempore omnis quisquam facere quod doloribus, nobis repellat.
+        Earum dicta non numquam sint voluptatibus est, accusantium adipisci dolorem nobis quae error tenetur laudantium rem, sapiente repellat provident minima quam veniam ab! Ipsum natus tempore non soluta, dolorem aperiam.
+        Beatae quas fugit nobis nisi ex nesciunt animi expedita unde eligendi ad, quos perferendis, veritatis quia temporibus, facere quasi a molestias cupiditate facilis fugiat consequatur deserunt qui vitae nemo. Ullam.</p>
+        <p>Eveniet culpa accusantium rem. Accusantium odio voluptatum ab aspernatur nam! Reprehenderit expedita quam velit, nemo ducimus iste veritatis illum nisi aperiam, voluptates asperiores enim corporis voluptas corrupti minima dolor earum.
+        Distinctio odio debitis sed molestias, obcaecati iure non nam quibusdam, natus quam eos velit fugiat asperiores recusandae nihil eaque ea? Eum, recusandae. Tempore enim recusandae iure sed officia nam quo!
+        Sit voluptate unde distinctio temporibus similique aut aliquam molestiae eos porro facere odio cupiditate recusandae placeat sequi magni dolorem, consequuntur obcaecati, eligendi reiciendis ipsa praesentium tempora magnam esse? Reiciendis, eaque.</p>
+        <p>Aliquam aut veniam vitae, illo molestias nobis enim ratione nesciunt beatae architecto odit quam cupiditate numquam sit consequuntur. Quia, rerum! Quisquam adipisci quae explicabo tempora, aliquam labore. Consequuntur, commodi explicabo!
+        Illo eaque natus ipsam ratione, in quas sunt porro voluptatibus, blanditiis soluta libero rerum hic fugit perferendis deserunt animi quos ullam similique, rem maiores. Placeat reiciendis veritatis reprehenderit deleniti esse.
+        Sunt, dolore corrupti aut ad commodi neque aliquam unde inventore alias, nam exercitationem tempore sequi iste ullam eum voluptate dignissimos maiores! Suscipit accusantium, unde earum quasi illo deserunt quam beatae.
+        Odit quidem consequuntur quibusdam iure enim facilis alias? Dolorem distinctio, consequatur omnis vero harum blanditiis eveniet! Sint autem dolores pariatur eos reprehenderit vero fugit, numquam, quis ex commodi, natus porro?</p>
+    </main>
+    <footer>
+        &copy; Все права защищены
+    </footer>
+</body>
+</html>
