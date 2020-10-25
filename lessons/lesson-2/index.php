aa5d4ee62556c5678baa473e3d16e684069b0f1a
Kiva Vlad <vladkiva9@gmail.com>
2020-10-25 16:42:39 +0300

    new update

--- lessons/lesson-2/index.php
+++ lessons/lesson-2/index.php
@@ -1,10 +1,13 @@
+<?php 
+error_reporting(E_ALL);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/lessons/style.css">
-    <title>Lesson-1</title>
+    <title>Lesson-2</title>
 </head>
 <body>
     <a href="/">Главная</a>
