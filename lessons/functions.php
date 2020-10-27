a56cde3c6b08ac8f6559002a3510bf234ca5976d
Kiva Vlad <vladkiva9@gmail.com>
2020-10-27 17:50:24 +0300

    last update

--- lessons/functions.php
+++ lessons/functions.php
@@ -1,9 +1,11 @@
 <?php
 
-function colorSearch(int $num, array $colors)
+function colorSearch(int $num)
 {
     $strNum = strval($num);
     $lenNum = strlen($strNum);
+
+    $colors = ['red' => 1, 'green' => 2, 'yellow' => 3, 'blue' => 4];
 
     if ($lenNum === 2) {
         $colorKeyIndex0 = array_search($strNum[0], $colors);
@@ -13,6 +15,16 @@ function colorSearch(int $num, array $colors)
         $colorIndex1 = $colorKeyIndex1 ?: "black";
 
         return [$colorIndex0, $colorIndex1];
+    } if ($lenNum === 3) {
+        $colorKeyIndex0 = array_search($strNum[0], $colors);
+        $colorKeyIndex1 = array_search($strNum[1], $colors);
+        $colorKeyIndex2 = array_search($strNum[2], $colors);
+
+        $colorIndex0 = $colorKeyIndex0 ?: "black";
+        $colorIndex1 = $colorKeyIndex1 ?: "black";
+        $colorIndex2 = $colorKeyIndex2 ?: "black";
+
+        return [$colorIndex0, $colorIndex1, $colorIndex2];
     } else {
         $colorKey = array_search($num, $colors);
 
--- lessons/lesson-2/index.php
+++ lessons/lesson-2/index.php
@@ -17,8 +17,6 @@ require $_SERVER['DOCUMENT_ROOT'] . '/lessons/functions.php';
     <table border="1">
         <?php
 
-        $colors = ['red' => 1, 'green' => 2, 'yellow' => 3, 'blue' => 4];
-
         for ($i = 1; $i <= 2; $i++) {
             echo "<tr>";
             $num1 = 1;
@@ -38,27 +36,58 @@ require $_SERVER['DOCUMENT_ROOT'] . '/lessons/functions.php';
 
                     if ($num1 > 9) {
 
-                        [$color1Index0, $color1Index1] = colorSearch($num1, $colors);
+                        [$color1Index0, $color1Index1] = colorSearch($num1);
+
+                        $num1Str = strval($num1);
+                        $num1Index0 = $num1Str[0];
+                        $num1Index1 = $num1Str[1];
                     } else {
-                        $colorNum1 = colorSearch($num1, $colors);
+                        $color1Index0 = colorSearch($num1);
+                        $color1Index1 = "black";
+                        $num1Index0 = $num1;
+                        $num1Index1 = "";
                     }
 
                     if ($num2 > 9) {
 
-                        [$color2Index0, $color2Index1] = colorSearch($num2, $colors);
+                        [$color2Index0, $color2Index1] = colorSearch($num2);
+
+                        $num2Str = strval($num2);
+                        $num2Index0 = $num2Str[0];
+                        $num2Index1 = $num2Str[1];
                     } else {
-                        $colorNum2 = colorSearch($num2, $colors);
+                        $color2Index0 = colorSearch($num2);
+                        $color2Index1 = "black";
+                        $num2Index0 = $num2;
+                        $num2Index1 = "";
                     }
 
-                    if ($res > 9) {
+                    if ($res > 9 && $res < 100) {
 
-                        [$colorResIndex0, $colorResIndex1] = colorSearch($res, $colors);
+                        [$colorResIndex0, $colorResIndex1] = colorSearch($res);
+
+                        $resStr = strval($res);
+                        $resIndex0 = $resStr[0];
+                        $resIndex1 = $resStr[1];
+                    } else if ($res >= 100) {
+                        [$colorResIndex0, $colorResIndex1, $colorResIndex2] = colorSearch($res);
+
+                        $resStr = strval($res);
+                        $resIndex0 = $resStr[0];
+                        $resIndex1 = $resStr[1];
+                        $resIndex2 = $resStr[2];
                     } else {
-                        $colorRes = colorSearch($res, $colors);
+                        $colorResIndex0 = colorSearch($res);
+                        $colorResIndex1 = "black";
+                        $colorResIndex2 = "black";
+                        $resIndex0 = $res;
+                        $resIndex1 = "";
+                        $resIndex2 = "";
                     }
 
-                    echo "<span style='color:{$colorNum1}'>{$num1}</span> × <span style='color:{$colorNum2}'>{$num2}</span> = 
-                    <span style='color:{$colorRes}'>{$res}</span></br>";
+                    echo "<span style='color:{$color1Index0}'>{$num1Index0}</span><span style='color:{$color1Index1}'>{$num1Index1}</span> × 
+                    <span style='color:{$color2Index0}'>{$num2Index0}</span><span style='color:{$color2Index1}'>{$num2Index1}</span> = 
+                    <span style='color:{$colorResIndex0}'>{$resIndex0}</span><span style='color:{$colorResIndex1}'>{$resIndex1}</span><span style='color:{$colorResIndex2}'>{$resIndex2}</span></br>";
                 }
 
                 echo "</td>";
